dccce2a306754665bbc627b6efe9267ab7879228
Michael Djorup <68293826+mdjorup@users.noreply.github.com>
2023-12-18 09:16:55 -0500

    formatted and fixed tailwind type issue (#3)

--- apps/advertiser-dash/tailwind.config.ts
+++ apps/advertiser-dash/tailwind.config.ts
@@ -2,10 +2,9 @@
 import sharedConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config: Pick<Config, "content" | "presets"> = {
   content: ["./src/app/**/*.tsx"],
-  presets: [sharedConfig]
+  presets: [sharedConfig],
 };
 
 export default config;
-
