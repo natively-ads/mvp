74c6bfa889a3a5bf19ff49404f33473b14f7cd52
mdjorup <michael.djorup@gmail.com>
2023-12-18 08:45:06 -0500

    tailwind setup for the advertiser dashboard

--- apps/advertiser-dash/src/app/page.tsx
+++ apps/advertiser-dash/src/app/page.tsx
@@ -1,5 +1,7 @@
-const Page = () => {
-  return <div>Page</div>;
-};
+ const Page = () => {
+  return (
+    <div>Page</div>
+  )
+}
 
-export default Page;
+export default Page
---
+++ apps/advertiser-dash/tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind config is required for editor support
+import sharedConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/app/**/*.tsx"],
+  presets: [sharedConfig]
+};
+
+export default config;
+
