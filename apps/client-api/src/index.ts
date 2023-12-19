5f2d64e579cbb8e1332f8699169ac3d045c00699
Michael Djorup <68293826+mdjorup@users.noreply.github.com>
2023-12-19 15:10:25 -0500

    API Setup (#5)

---
+++ apps/client-api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+console.log("process.env.CLIENT_API_PORT: ", process.env.CLIENT_API_PORT);
+
+const port = process.env.CLIENT_API_PORT || 3001;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
