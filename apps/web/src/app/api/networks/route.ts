b4d51f68e9a1da0d70375160b8dd95a946c30a55
mdjorup <michael.djorup@gmail.com>
2024-01-10 19:01:38 -0500

    POST request to create network implemented

--- apps/web/src/app/api/networks/route.ts
+++ apps/web/src/app/api/networks/route.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/app/util/supabase/server';
+import { AdSchema, Network } from '@repo/types';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -20,7 +21,57 @@ export async function GET(req: NextRequest) {
 	const rawNetworks = await query;
 
 	const networksJson = rawNetworks.data ?? [];
-	return NextResponse.json({ Result: networksJson });
+	return NextResponse.json({ data: networksJson });
 }
 
-export async function POST(req: NextRequest) {}
+interface CreateNetworkRequestBody {
+	publisherId: Network['publisherId'];
+	name: Network['name'];
+	description: Network['description'];
+	reservePrice: Network['reservePrice'];
+	adSchema: AdSchema;
+}
+
+const isValidAdSchema = (adSchema: AdSchema): boolean => {
+	for (const adField of adSchema) {
+		if (adField.type === 'string' && typeof adField.value !== 'string') {
+			return false;
+		} else if (adField.type === 'number' && typeof adField.value !== 'number') {
+			return false;
+		} else if (
+			adField.type === 'boolean' &&
+			typeof adField.value !== 'boolean'
+		) {
+			return false;
+		}
+		// Additional type checks can be added here if necessary
+	}
+
+	return true;
+};
+
+export async function POST(request: NextRequest) {
+	const requestBody = (await request.json()) as CreateNetworkRequestBody;
+	const { publisherId, name, description, reservePrice, adSchema } =
+		requestBody;
+
+	if (!isValidAdSchema(adSchema)) {
+		return NextResponse.json({ error: 'Invalid ad schema' }, { status: 400 });
+	}
+
+	const cookieStore = cookies();
+	const client = createClient(cookieStore);
+
+	const rawNetwork = await client
+		.from('networks')
+		.insert({
+			publisherId,
+			name,
+			description,
+			reservePrice,
+			adFormat: adSchema,
+		});
+
+	const networkJson = rawNetwork.data ?? {};
+	return NextResponse.json({ data: networkJson });
+}
