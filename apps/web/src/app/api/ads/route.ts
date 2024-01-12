e0c4b6b8fdce3aa33895484159968a6c9d605c0a
Michael Djorup <68293826+mdjorup@users.noreply.github.com>
2024-01-12 09:20:36 -0500

    Finishing Up Core API (#24)

--- apps/web/src/app/api/ads/route.ts
+++ apps/web/src/app/api/ads/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@/app/util/supabase/server';
+import { Ad } from '@repo/types';
 import { cookies } from 'next/headers';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
 	const cookieStore = cookies();
@@ -8,51 +9,55 @@ export async function GET(request: NextRequest) {
 
 	const url = new URL(request.url);
 	const advertiserId = url.searchParams.get('advertiserId');
+	const networkId = url.searchParams.get('networkId');
 
-	const rawAds = await client
-		.from('ads')
-		.select('*')
-		.eq('advertiserId', advertiserId);
+	let query = client.from('ads').select('*');
+
+	if (advertiserId != null) {
+		query = query.eq('advertiserId', advertiserId);
+	}
+	if (networkId != null) {
+		query = query.eq('networkId', networkId);
+	}
+
+	const rawAds = await query;
+
 	const adsJson = rawAds.data ?? [];
 	return NextResponse.json(adsJson);
 }
 
+interface CreateAdRequestBody {
+	networkId: Ad['networkId'];
+	advertiserId: Ad['advertiserId'];
+	content: Ad['content'];
+}
+
 export async function POST(request: NextRequest) {
-	try {
-		const cookieStore = cookies();
-		const client = createClient(cookieStore);
+	const cookieStore = cookies();
+	const client = createClient(cookieStore);
 
-		const body = await request.json();
+	const body = (await request.json()) as CreateAdRequestBody;
 
-		if (!body) {
-			throw new Error('Missing Body');
-		}
-
-		const { networkId, content, advertiserId } = body;
-
-		const { error } = await client
-			.from('ads')
-			.insert([
-				{
-					networkId: networkId,
-					advertiserId: advertiserId,
-					content: content,
-				},
-			])
-			.select();
-
-		if (error) {
-			throw error;
-		}
-		return NextResponse.json('Success');
-	} catch (err: any) {
-		return new NextResponse(JSON.stringify({ error: err.message }), {
-			status: 500,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
+	if (body == null) {
+		throw new Error('Missing Body');
 	}
+
+	const { networkId, content, advertiserId } = body;
+
+	const { data, error } = await client
+		.from('ads')
+		.insert({
+			networkId: networkId,
+			advertiserId: advertiserId,
+			content: content,
+		})
+		.select();
+
+	if (error != null || data == null) {
+		return NextResponse.json(error, { status: 500 });
+	}
+
+	return NextResponse.json(data, { status: 201 });
 }
 
 export async function DELETE(request: NextRequest) {
--- apps/web/src/app/api/campaigns/route.ts
+++ apps/web/src/app/api/campaigns/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@/app/util/supabase/server';
+import { Campaign } from '@repo/types';
 import { cookies } from 'next/headers';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
 	const cookieStore = cookies();
@@ -8,54 +9,68 @@ export async function GET(request: NextRequest) {
 
 	const url = new URL(request.url);
 	const advertiserId = url.searchParams.get('advertiserId');
+	const networkId = url.searchParams.get('networkId');
+	const adId = url.searchParams.get('adId');
 
-	const rawCampaigns = await client
-		.from('campaigns')
-		.select('*')
-		.eq('advertiserId', advertiserId);
+	let query = client.from('campaigns').select('*');
+
+	if (advertiserId != null) {
+		query = query.eq('advertiserId', advertiserId);
+	}
+	if (networkId != null) {
+		query = query.eq('networkId', networkId);
+	}
+	if (adId != null) {
+		query = query.eq('adId', adId);
+	}
+
+	const rawCampaigns = await query;
+
 	const campaignsJson = rawCampaigns.data ?? [];
 	return NextResponse.json(campaignsJson);
 }
 
+interface CreateCampaignRequestBody {
+	networkId: Campaign['networkId'];
+	advertiserId: Campaign['advertiserId'];
+	adId: Campaign['adId'];
+	title: Campaign['title'];
+	budget: Campaign['budget'];
+	cpmBid: Campaign['cpmBid'];
+}
+
 export async function POST(request: NextRequest) {
-	try {
-		const cookieStore = cookies();
-		const client = createClient(cookieStore);
+	const cookieStore = cookies();
+	const client = createClient(cookieStore);
 
-		const body = await request.json();
+	const body = (await request.json()) as CreateCampaignRequestBody;
 
-		if (!body) {
-			throw new Error('Missing Body');
-		}
-
-		const { networkId, adId, title, budget, cpmBid, advertiserId } = body;
-
-		const { error } = await client
-			.from('campaigns')
-			.insert([
-				{
-					networkId: networkId,
-					advertiserId: advertiserId,
-					adId: adId,
-					budget: budget,
-					cpmBid: cpmBid,
-					title: title,
-				},
-			])
-			.select();
-
-		if (error) {
-			throw error;
-		}
-		return NextResponse.json('Success');
-	} catch (err: any) {
-		return new NextResponse(JSON.stringify({ error: err.message }), {
-			status: 500,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
+	if (!body) {
+		throw new Error('Missing Body');
 	}
+
+	const { networkId, adId, title, budget, cpmBid, advertiserId } = body;
+
+	const { data, error } = await client
+		.from('campaigns')
+		.insert({
+			networkId: networkId,
+			advertiserId: advertiserId,
+			adId: adId,
+			budget: budget,
+			cpmBid: cpmBid,
+			title: title,
+		})
+		.select();
+
+	if (error != null || data == null) {
+		return NextResponse.json(
+			{ error: 'Error creating campaign' },
+			{ status: 500 },
+		);
+	}
+
+	return NextResponse.json(data, { status: 200 });
 }
 
 export async function DELETE(request: NextRequest) {
